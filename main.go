c39e193791f2d5e0be4c2494148fd87b9b548c2a
Andrey <andreyacostar@gmail.com>
2024-09-17 15:12:53 -0600

    feat: add cli structure and storage

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Print("Hello, World!")
+	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	todos.add("Buy milk")
+	todos.add("Buy eggs")
+	todos.add("Buy bread")
+	todos.add("Buy butter")
+	todos.delete(2)
+	todos.print()
+	storage.Save(todos)
 
 }
---
+++ todo.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:     title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index >= 0 && index < len(*todos) {
+		err := errors.New("Index out of range")
+		fmt.Println(err.Error())
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	*todos = append((*todos)[:index], (*todos)[index+1:]...)
+	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	(*todos)[index].Completed = !(*todos)[index].Completed
+	if (*todos)[index].Completed {
+		(*todos)[index].CompletedAt = time.Now()
+	} else {
+		(*todos)[index].CompletedAt = time.Time{}
+	}
+	return nil
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	(*todos)[index].Title = title
+	return nil
+}
+
+func (todos *Todos) print() {
+	for i, todo := range *todos {
+		fmt.Printf("%d: %s\n", i, todo.Title)
+	}
+}
